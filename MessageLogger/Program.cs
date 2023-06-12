652f7e865b2028c08ad8562b08904ea59c5a75b1
jkimball54 <jeremyck54@gmail.com>
2023-06-12 11:24:00 -0600

    Annotated Program.cs for iteration 2 plans

--- MessageLogger/Program.cs
+++ MessageLogger/Program.cs
@@ -1,9 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MessageLogger.Model;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+
+
+//Wrap in using statement to create db connection
 
 Console.WriteLine("Welcome to Message Logger!");
 Console.WriteLine();
-Console.WriteLine("Let's create a user pofile for you.");
+Console.WriteLine("Let's create a user profile for you.");
 Console.Write("What is your name? ");
 string name = Console.ReadLine();
 Console.Write("What is your username? (one word, no spaces!) ");
@@ -17,15 +22,17 @@ Console.WriteLine();
 Console.Write("Add a message (or `quit` to exit): ");
 
 string userInput = Console.ReadLine();
-List<User> users = new List<User>() { user };
+List<User> users = new List<User>() { user }; //remove & add user to db instead
 
 while (userInput.ToLower() != "quit")
 {
+    //message creation
     while (userInput.ToLower() != "log out")
     {
-        user.Messages.Add(new Message(userInput));
+        user.Messages.Add(new Message(userInput)); //add message to db, reference user from db instead
+        //savechanges()
 
-        foreach (var message in user.Messages)
+        foreach (var message in user.Messages) //update to reference database
         {
             Console.WriteLine($"{user.Name} {message.CreatedAt:t}: {message.Content}");
         }
@@ -36,6 +43,9 @@ while (userInput.ToLower() != "quit")
         Console.WriteLine();
     }
 
+
+    //switching user
+    //new
     Console.Write("Would you like to log in a `new` or `existing` user? Or, `quit`? ");
     userInput = Console.ReadLine();
     if (userInput.ToLower() == "new")
@@ -45,22 +55,24 @@ while (userInput.ToLower() != "quit")
         Console.Write("What is your username? (one word, no spaces!) ");
         username = Console.ReadLine();
         user = new User(name, username);
-        users.Add(user);
+        users.Add(user); //add to database instead
+        //savechanges()
         Console.Write("Add a message: ");
 
         userInput = Console.ReadLine();
 
     }
+    //existing
     else if (userInput.ToLower() == "existing")
     {
         Console.Write("What is your username? ");
         username = Console.ReadLine();
         user = null;
-        foreach (var existingUser in users)
+        foreach (var existingUser in users) //in database instead of list
         {
             if (existingUser.Username == username)
             {
-                user = existingUser;
+                user = existingUser; //user = database user
             }
         }
         
@@ -79,8 +91,9 @@ while (userInput.ToLower() != "quit")
 
 }
 
+//outro message
 Console.WriteLine("Thanks for using Message Logger!");
-foreach (var u in users)
+foreach (var u in users) //in context instead
 {
     Console.WriteLine($"{u.Name} wrote {u.Messages.Count} messages.");
 }
