fca3fe7aafa795d1bb72f390948eed2a51824db4
jkimball54 <jeremyck54@gmail.com>
2023-06-15 09:08:09 -0600

    Finished ready for presentation

---
+++ MessageLogger.Tests/PromptTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageLogger.Tests
+{
+    public class PromptTest
+    {
+        [Fact]
+        public void TwelveHourTime_ReturnsCorrectTimeFormat()
+        {
+            //unfortunately ran out of time
+        }
+    }
+}
